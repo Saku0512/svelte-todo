7a860c90af6b8e86a15280a029650b01e467077b
saku0512 <saku0512sec@gmail.com>
2026-03-15 13:49:10 +0900

    feat: /create api

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,3 +1,10 @@
 import Database from "better-sqlite3";
 
 export const db = new Database("todo.db");
+
+db.prepare(`
+CREATE TABLE IF NOT EXISTS todos (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    text TEX NOT NULL,
+    done INTEGER NOT NULL DEFAULT 0)
+`).run();
